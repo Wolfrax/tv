5fffe093792494608bbccd2c4a6c847aa96a026b
mm <mats.melander@gmail.com>
2021-07-22 17:37:31 +0200

    Use new API, WeatherMeasurepoint, instead of WeatherStation API (will be depreciated)

--- ws.py
+++ ws.py
@@ -10,7 +10,6 @@ from sseclient import SSEClient
 import json
 import logging
 import dateutil
-from dateutil.parser import parse
 import matplotlib.pyplot as plt
 import matplotlib
 from windrose import WindroseAxes
@@ -53,16 +52,14 @@ print("Station: {}".format(stn_name))
 query = """
 <REQUEST>
   <LOGIN authenticationkey='""" + auth_key + """' />
-  <QUERY sseurl="true" objecttype='WeatherStation' schemaversion='1'>
-    <INCLUDE>Measurement.Air.RelativeHumidity</INCLUDE>
-    <INCLUDE>Measurement.Air.Temp</INCLUDE>  
-    <INCLUDE>Measurement.MeasureTime</INCLUDE>    
-    <INCLUDE>Measurement.Precipitation.Amount</INCLUDE>
-    <INCLUDE>Measurement.Precipitation.AmountName</INCLUDE>
-    <INCLUDE>Measurement.Precipitation.Type</INCLUDE>
-    <INCLUDE>Measurement.Wind.Direction</INCLUDE>
-    <INCLUDE>Measurement.Wind.Force</INCLUDE>
-    <INCLUDE>Measurement.Wind.ForceMax</INCLUDE>
+  <QUERY sseurl="true" objecttype='WeatherMeasurepoint' schemaversion='1'>
+    <INCLUDE>Observation.Sample</INCLUDE>  
+    <INCLUDE>Observation.Air.RelativeHumidity.Value</INCLUDE>
+    <INCLUDE>Observation.Air.Temperature.Value</INCLUDE>
+    <INCLUDE>Observation.Wind.Direction.Value</INCLUDE>
+    <INCLUDE>Observation.Wind.Speed.Value</INCLUDE>
+    <INCLUDE>Observation.Aggregated10minutes.Precipitation.TotalWaterEquivalent.Value</INCLUDE>
+    <INCLUDE>Observation.Aggregated30minutes.Wind.SpeedMax.Value</INCLUDE>
     <INCLUDE>Name</INCLUDE>
     <FILTER>
         <EQ name="Name" value='""" + stn_name + """' />
@@ -131,27 +128,31 @@ class Graph:
         self.wr_ax.set_title("Wind force and directions")
 
     def plot(self, data):
-        time = [dateutil.parser.parse(msr["MeasureTime"]) for msr in data]
-        temp = [msr["Air"]["Temp"] for msr in data]
-        hum = [msr["Air"]["RelativeHumidity"] for msr in data]
-        rain = [msr["Precipitation"]["Amount"] for msr in data]
+        time = [dateutil.parser.parse(msr["Sample"]) for msr in data]
+        temp = [msr["Air"]["Temperature"]["Value"] for msr in data]
+        hum = [msr["Air"]["RelativeHumidity"]["Value"] for msr in data]
+
+        rain = [msr['Aggregated10minutes']['Precipitation']['TotalWaterEquivalent']['Value'] for msr in data]
         rain_hourly = [msr["Precipitation"]["Hourly"] for msr in data]
         # acc_rain = [round(sum(rain[:i]), 1) for i in range(1, len(rain) + 1)]
         acc_rain_hourly = [round(sum(rain_hourly[:i]), 1) for i in range(1, len(rain) + 1)]
-        wind_dir = [msr["Wind"]["Direction"] if not math.isnan(msr["Wind"]["Direction"]) else 0 for msr in data]
-        wind_force = [msr["Wind"]["Force"] if not math.isnan(msr["Wind"]["Force"]) else 0 for msr in data]
-        wind_force_max = [msr["Wind"]["ForceMax"] for msr in data]
 
-        ts1 = dateutil.parser.parse(data[0]["MeasureTime"])
-        wind_dir_hour = [data[0]["Wind"]["Direction"]]
+        wind_dir = [msr["Wind"][0]["Direction"]["Value"]
+                    if not math.isnan(msr["Wind"][0]["Direction"]["Value"]) else 0 for msr in data]
+        wind_force = [msr["Wind"][0]["Speed"]["Value"]
+                      if not math.isnan(msr["Wind"][0]["Speed"]["Value"]) else 0 for msr in data]
+        wind_force_max = [msr["Aggregated30minutes"]["Wind"]["SpeedMax"]["Value"] for msr in data]
+
+        ts1 = dateutil.parser.parse(data[0]["Sample"])
+        wind_dir_hour = [data[0]["Wind"][0]["Direction"]["Value"]]
         wind_time_hour = [ts1]
-        wind_val_hour = [data[0]["Wind"]["Force"]]
+        wind_val_hour = [data[0]["Wind"][0]["Speed"]["Value"]]
         for msr in data:
-            ts2 = dateutil.parser.parse(msr["MeasureTime"])
+            ts2 = dateutil.parser.parse(msr["Sample"])
             if (ts2 - ts1).total_seconds() / 3600.0 > 1.0:
-                wind_dir_hour.append(msr["Wind"]["Direction"])
+                wind_dir_hour.append(msr["Wind"][0]["Direction"]["Value"])
                 wind_time_hour.append(ts2)
-                wind_val_hour.append(msr["Wind"]["Force"])
+                wind_val_hour.append(msr["Wind"][0]["Speed"]["Value"])
                 ts1 = ts2
 
         self.ax1.plot(time, temp)
@@ -235,66 +236,78 @@ class Measurements:
 
     def _rain(self, msr):
         if self.data:
-            dt = dateutil.parser.parse(msr['MeasureTime'])
-            prev_dt = dateutil.parser.parse(self.data[-1]['MeasureTime'])
+            dt = dateutil.parser.parse(msr['Sample'])
+            prev_dt = dateutil.parser.parse(self.data[-1]['Sample'])
             delta = (dt - prev_dt).total_seconds()
             if delta < 0:
                 # Note, if delta < 0, we are traversing a list of data without 'Hourly'-field
                 # in Precipitation (called from __init__) -> return 0
                 logger.warning("Rain - delta less than zero: {}".format(delta))
-                return float("Nan")
+                return 0
             elif delta == 0:
                 # Current and previous timestamps are equal
                 logger.warning("Rain - delta is zero")
                 return 0
             else:
-                # Calculate rain amount per hour, assuming 'Amount' is _intensity_ of rain
-                # Eg if Amount == 1.0 and previous reading were 10 minutes ago, it has rained 1 / 10min this period
-                return msr['Precipitation']['Amount'] *  (delta / 3600.0)
+                # Calculate rain amount per hour, sum av the last 6 10 minutes periods, assuming we get a new
+                # reading every 10 minute
+                sum = 0
+                for elem in self.data[-6:]:
+                    sum += elem['Aggregated10minutes']['Precipitation']['TotalWaterEquivalent']['Value']
+                return sum
+
         else:
-            return float("Nan")
+            return 0
 
     def _check(self, msr):
         # Fields might be missing, set to default values
+        if 'Aggregated10minutes' not in msr:
+            msr['Aggregated10minutes'] = {}
+        if 'Precipitation' not in msr['Aggregated10minutes']:
+            msr['Aggregated10minutes']['Precipitation'] =  {'TotalWaterEquivalent': {'Value': 0}}
+        if 'TotalWaterEquivalent' not in msr['Aggregated10minutes']['Precipitation']:
+            msr['Aggregated10minutes']['Precipitation']['TotalWaterEquivalent'] = {'Value': 0}
+
+        if 'Aggregated30minutes' not in msr:
+            msr['Aggregated30minutes'] = {}
+        if 'Wind' not in msr['Aggregated30minutes']:
+            msr['Aggregated30minutes']['Wind'] = {'SpeedMax': {'Value': 0}}
+        if 'SpeedMax' not in msr['Aggregated30minutes']['Wind']:
+            msr['Aggregated30minutes']['Wind']['SpeedMax'] = {'Value': 0}
+
         if 'Precipitation' not in msr:
             msr['Precipitation'] = {}
-        if 'Amount' not in msr['Precipitation']:
-            msr['Precipitation']['Amount'] = 0  # TV doesn't include Amount value if it is not raining :-(
-        if 'AmountName' not in msr['Precipitation']:
-            msr['Precipitation']['AmountName'] = ""
-        if 'Type' not in msr['Precipitation']:
-            msr['Precipitation']['Type'] = ""
         if 'Hourly' not in msr['Precipitation']:
             msr['Precipitation']['Hourly'] = self._rain(msr)
 
         if 'Air' not in msr:
             msr['Air'] = {}
-        if 'Temp' not in msr['Air']:
-            msr['Air']['Temp'] = float("NaN")
+        if 'Temperature' not in msr['Air']:
+            msr['Air']['Temperature'] = {'Value': float("NaN")}
         if 'RelativeHumidity' not in msr['Air']:
-            msr['Air']['RelativeHumidity'] = float("NaN")
+            msr['Air']['RelativeHumidity'] = {'Value': float("NaN")}
 
         if 'Wind' not in msr:
-            msr['Wind'] = {}
-        if 'Direction' not in msr['Wind']:
-            msr['Wind']['Direction'] = float("NaN")
-        if 'Force' not in msr['Wind']:
-            msr['Wind']['Force'] = float("NaN")
-        if 'ForceMax' not in msr['Wind']:
-            msr['Wind']['ForceMax'] = float("Nan")
+            msr['Wind'] = [{}]
+        if not msr['Wind']:
+            msr['Wind'].append({'Direction': {'Value': float("NaN")}})
+        if 'Direction' not in msr['Wind'][0]:
+            msr['Wind'][0]['Direction'] = {'Value': float("NaN")}
+        if 'Speed' not in msr['Wind'][0]:
+            msr['Wind'][0]['Speed'] = {'Value': float("NaN")}
 
     def add(self, d):
-        if 'Measurement' not in d['RESPONSE']['RESULT'][0]['WeatherStation'][0]:
+        if 'Observation' not in d['RESPONSE']['RESULT'][0]['WeatherMeasurepoint'][0]:
             logger.warning("Corrupt measurement data - no Measurement")
             return  # Unlikely, but got a corrupt data element, do nothing
 
-        head = d['RESPONSE']['RESULT'][0]['WeatherStation'][0]['Measurement']
-        ts = head['MeasureTime']
+        head = d['RESPONSE']['RESULT'][0]['WeatherMeasurepoint'][0]['Observation']
+        ts = head['Sample']
         dt = dateutil.parser.parse(ts)
         if self.data:
             # List will only include items the last 24 hours
             self.data = [m for m in self.data
-                         if (dt - dateutil.parser.parse(m['MeasureTime'])).total_seconds() / 3600 <= 24.0]
+                         if (dt - dateutil.parser.parse(m['Sample'])).total_seconds() / 3600 <= 24.0]
 
         # Check head for missing fields, not all fields are included in TV responses :-(
         self._check(head)
@@ -330,7 +343,7 @@ if __name__ == "__main__":
     # handler.setLevel(logging.DEBUG)
     # logger.addHandler(handler)
 
-    logger.info("Start v0.9")
+    logger.info("Start v1.0")
     logger.info("Station: {}".format(stn_name))
 
     r = requests.post(url, data=query.encode('utf-8'), headers={'Content-Type': 'text/xml'}).json()
@@ -365,39 +378,31 @@ if __name__ == "__main__":
                         if p.is_alive(): p.kill()
 
                     with app.app_context():
-                        first_time = dateutil.parser.parse(msr.data[0]["MeasureTime"])
-                        last_time = dateutil.parser.parse(msr.data[-1]["MeasureTime"])
+                        first_time = dateutil.parser.parse(msr.data[0]["Sample"])
+                        last_time = dateutil.parser.parse(msr.data[-1]["Sample"])
 
                         # Boring string management for presentation..., the ones with suffix '_str' is visualized
                         h2_str = first_time.strftime("%Y-%m-%d %H:%M:%S") + " - " + \
                                  last_time.strftime("%Y-%m-%d %H:%M:%S")
 
-                        air_temp_str = str(msr.data[-1]["Air"]["Temp"]) + "°C" \
-                            if not math.isnan(msr.data[-1]["Air"]["Temp"]) else ""
-                        air_relhum_str = str(msr.data[-1]["Air"]["RelativeHumidity"]) + "%" \
-                            if not math.isnan(msr.data[-1]["Air"]["RelativeHumidity"]) else ""
+                        air_temp_str = str(msr.data[-1]["Air"]["Temperature"]["Value"]) + "°C" \
+                            if not math.isnan(msr.data[-1]["Air"]["Temperature"]["Value"]) else ""
+                        air_relhum_str = str(msr.data[-1]["Air"]["RelativeHumidity"]["Value"]) + "%" \
+                            if not math.isnan(msr.data[-1]["Air"]["RelativeHumidity"]["Value"]) else ""
 
-                        rain_amount = str(msr.data[-1]["Precipitation"]["Amount"]) + "mm" \
-                            if not math.isnan(msr.data[-1]["Precipitation"]["Amount"]) else ""
-                        if rain_amount != "":
-                            rain_type = msr.data[-1]["Precipitation"]["Type"]
-                            rain_name = msr.data[-1]["Precipitation"]["AmountName"]
-                            rain_descr = rain_type + " - " + rain_name if rain_type != rain_name else rain_type
-                            rain_str = rain_amount + " (" + rain_descr + ")"
-                        else:
-                            rain_str = ""
+                        rain_str = ""  # Old stuff, not used anymore
 
-                        bin = numpy.digitize(msr.data[-1]["Wind"]["Direction"], bins)
+                        bin = numpy.digitize(msr.data[-1]["Wind"][0]["Direction"]["Value"], bins)
                         # bin can be out of index if "Direction" == "NaN"
                         wd_label = WIND_LABELS[bin] if bin < len(WIND_LABELS) else ""
 
-                        wd_dir = str(msr.data[-1]["Wind"]["Direction"]) + "°" \
-                            if not math.isnan(msr.data[-1]["Wind"]["Direction"]) else ""
+                        wd_dir = str(msr.data[-1]["Wind"][0]["Direction"]["Value"]) + "°" \
+                            if not math.isnan(msr.data[-1]["Wind"][0]["Direction"]["Value"]) else ""
                         wd_label_dir_str = wd_label if wd_dir == "" else wd_label + " (" + wd_dir + ")"
-                        wd_force_str = str(msr.data[-1]["Wind"]["Force"]) + "m/s" \
-                            if not math.isnan(msr.data[-1]["Wind"]["Force"]) else ""
-                        wd_force_max_str = str(msr.data[-1]["Wind"]["ForceMax"]) + "m/s" \
-                            if not math.isnan(msr.data[-1]["Wind"]["ForceMax"]) else ""
+                        wd_force_str = str(msr.data[-1]["Wind"][0]["Speed"]["Value"]) + "m/s" \
+                            if not math.isnan(msr.data[-1]["Wind"][0]["Speed"]["Value"]) else ""
+                        wd_force_max_str = str(msr.data[-1]["Aggregated30minutes"]["Wind"]["SpeedMax"]["Value"]) + "m/s" \
+                            if not math.isnan(msr.data[-1]["Aggregated30minutes"]["Wind"]["SpeedMax"]["Value"]) else ""
 
                         html_file = render_template('ws_template.html',
                                                     title=stn_name,
